7c991e2ab9eec81fe1a7c47d489c7b38e2fe0db8
roma_marshall <roma_marshall@outlook.com>
2023-06-10 02:00:19 +0200

    final fix

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 function clockTick() {
     let currentTime = new Date(), hours, minutes, seconds
 
-    currentTime.getHours() < 10 ? hours = '0' + currentTime.getHours() : hours =  currentTime.getHours()
+    // currentTime.getHours() < 10 ? hours = '0' + currentTime.getHours() : hours =  currentTime.getHours()
+    hours =  currentTime.getHours()
     currentTime.getMinutes() < 10 ? minutes = '0' + currentTime.getMinutes() : minutes =  currentTime.getMinutes()
     currentTime.getSeconds() < 10 ? seconds = '0' + currentTime.getSeconds() : seconds =  currentTime.getSeconds()
     document.getElementById('date').innerHTML = (hours + ':' + minutes + ':' + seconds)
