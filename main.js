85798dd8d46a1f5ffbb24deb0e88908a6e5049ce
vires in numeris <79694559+roma-marshall@users.noreply.github.com>
2023-06-19 11:12:56 +0200

    update timezone

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 function clockTick() {
-    let currentTime = new Date(), hours, minutes, seconds
+    let date = new Date()
 
+    date = date.toLocaleString('de-DE', {
+        timeZone: 'Europe/Berlin',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+    })
+    
+    document.getElementById('date').innerHTML = date
+
+    
+    // let currentTime = new Date(), hours, minutes, seconds
     // currentTime.getHours() < 10 ? hours = '0' + currentTime.getHours() : hours =  currentTime.getHours()
-    hours =  currentTime.getHours()
-    currentTime.getMinutes() < 10 ? minutes = '0' + currentTime.getMinutes() : minutes =  currentTime.getMinutes()
-    currentTime.getSeconds() < 10 ? seconds = '0' + currentTime.getSeconds() : seconds =  currentTime.getSeconds()
-    document.getElementById('date').innerHTML = (hours + ':' + minutes + ':' + seconds)
+    // hours =  currentTime.getHours()
+    // currentTime.getMinutes() < 10 ? minutes = '0' + currentTime.getMinutes() : minutes =  currentTime.getMinutes()
+    // currentTime.getSeconds() < 10 ? seconds = '0' + currentTime.getSeconds() : seconds =  currentTime.getSeconds()
+    // document.getElementById('date').innerHTML = (hours + ':' + minutes + ':' + seconds)
 }
 setInterval(clockTick, 1000);
 
