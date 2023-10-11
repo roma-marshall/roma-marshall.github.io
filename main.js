1dccc890befd12994a272e047fe61b3e935fa30e
roma_marshall <roma_marshall@outlook.com>
2023-10-11 23:28:27 +0200

    refactoring

--- main.js
+++ main.js
@@ -1,33 +1,20 @@
-function clockTick() {
+setInterval(() => {
     let date = new Date()
-
     date = date.toLocaleString('de-DE', {
         timeZone: 'Europe/Berlin',
         hour: 'numeric',
         minute: 'numeric',
         second: 'numeric'
     })
-    
     document.getElementById('date').innerHTML = date
-
-    
-    // let currentTime = new Date(), hours, minutes, seconds
-    // currentTime.getHours() < 10 ? hours = '0' + currentTime.getHours() : hours =  currentTime.getHours()
-    // hours =  currentTime.getHours()
-    // currentTime.getMinutes() < 10 ? minutes = '0' + currentTime.getMinutes() : minutes =  currentTime.getMinutes()
-    // currentTime.getSeconds() < 10 ? seconds = '0' + currentTime.getSeconds() : seconds =  currentTime.getSeconds()
-    // document.getElementById('date').innerHTML = (hours + ':' + minutes + ':' + seconds)
-}
-setInterval(clockTick, 1000);
+}, 1000)
 
 
-function copyEmail() {
-    let copyText = document.getElementById("email");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    navigator.clipboard.writeText(copyText.value);
-
-    // alert("Copied the text: " + copyText.value);
+const copyEmail = () => {
+    let copyText = document.getElementById("email")
+    copyText.select()
+    copyText.setSelectionRange(0, 99999)
+    navigator.clipboard.writeText(copyText.value)
 }
 
 addEventListener('DOMContentLoaded', async () => {
