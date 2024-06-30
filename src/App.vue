7be2754eb909f78d25fe7c5b6ebc4d225a2160ac
roma_marshall <roma_marshall@outlook.com>
2024-06-30 14:16:58 +0200

    hideEmail method

--- src/App.vue
+++ src/App.vue
@@ -36,26 +36,41 @@
         </div>
         <div class="md:w-3/4 space-y-2">
           <div v-for="child in item.content">
-            <p class="mb-2 space-x-2">
+            <p class="mb-2 space-x-2" v-if="isEmail || child.text != 'Email'">
               <span>{{ child.text }}</span>
               <a :href="child.link" target="_blank" class="underline hover:opacity-60">{{ child.text2 }}</a>
             </p>
           </div>
         </div>
       </div>
-
-
-
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-
+import { ref, onMounted } from 'vue'
 import data from './data/data.js'
 import data2 from './data/data2.js'
 import data3 from './data/data3.js'
 
+const isEmail = ref(false)
 
+const fetchData = async () => {
+  const url = 'https://api.ipify.org?format=json'
+  const response = await fetch(url)
+  const data = await response.json()
+
+  const url2 = `https://api.ipbase.com/v1/json/${data.ip}`
+  const response2 = await fetch(url2)
+  const data2 = await response2.json()
+
+  isEmail.value = !(
+      data.ip
+      && (data2['country_code'] !== 'DE'
+      && data2['country_code'] !== 'CH'
+      && data2['country_code'] !== 'AT')
+  )
+}
+
+onMounted(() => fetchData())
 </script>
