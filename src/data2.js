db40da962a1c53c2800c720b5247933af4afc08e
roma_marshall <roma_marshall@outlook.com>
2024-06-30 13:29:44 +0200

    added education

--- src/data2.js
+++ src/data2.js
@@ -12,7 +12,7 @@ export default [
         text: 'Blackbit digital Commerce GmbH',
         date: '2022 – Now',
         description: 'Clients: BTC Business Technology Consulting AG, Bikeleasing-Service GmbH, CRB Analyse Service GmbH, EXPRESSO Deutschland GmbH',
-        link: 'https://www.blackbit.com/en'
+        link: 'https://www.blackbit.com'
       },
       {
         text: 'Upwork Inc.',
@@ -24,7 +24,7 @@ export default [
         text: 'AMEFA GmbH',
         date: '2021 – 2022',
         description: 'Full-time Frontend Developer',
-        link: 'https://www.amefa-med.com/home',
+        link: 'https://www.amefa-med.com',
       },
       {
         text: 'Browserwerk GmbH',
@@ -32,6 +32,35 @@ export default [
         description: 'Clients: NASPA Immobilen GmbH, Deutsche Lufthansa AG, KEGON AG, Degussa Bank AG, VAN HEES GmbH',
         link: 'https://www.browserwerk.de',
       }
+    ]
+  },
+  {
+    title: 'Education',
+    content: [
+      {
+        text: 'Pimcore Academy',
+        date: '2023',
+        description: '',
+        link: 'https://pimcore.com/en/developers/academy'
+      },
+      {
+        text: 'Refactoring UI by Adam Wathan',
+        date: '2022',
+        description: '',
+        link: 'https://www.refactoringui.com'
+      },
+      {
+        text: 'Friedrich Ebert School',
+        date: '2018 – 2021',
+        description: 'Computer Science, App Development',
+        link: 'https://fes-wiesbaden.de'
+      },
+      {
+        text: 'Municipality of Worms',
+        date: '2016 – 2017',
+        description: 'Internship: IT Department',
+        link: 'https://www.worms.de'
+      },
     ]
   }
 ]
